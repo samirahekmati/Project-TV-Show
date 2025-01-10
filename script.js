558a597b5ea7a0d7c56c928b10d48e586209a3ca
gai93003 <deng93003@gmail.com>
2025-01-10 22:13:54 +0000

    These are some of the changes i have made to make the search engine work.

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 //You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
+  searchEpisodes();
   makePageForEpisodes(allEpisodes);
 }
 
+const inputEl = document.getElementById('input-el');
+const searchBtn = document.querySelector('button');
+
+function searchEpisodes() {
+  const allEpisodes = getAllEpisodes();
+
+  inputEl.addEventListener('input', () => {
+    const searchedWord = inputEl.value.toLowerCase();
+    const matchingEpisodes = allEpisodes.filter(episode => {
+      return episode.name.toLowerCase().includes(searchedWord) || episode.summary.toLowerCase().includes(searchedWord);
+    });
+
+    makePageForEpisodes(matchingEpisodes);
+  })
+}
+
 function makePageForEpisodes(episodeList) {
+  const main = document.getElementById('all-episodes');
+  main.innerHTML = '';
   for (const episode of episodeList) {
     const filmCard = document
       .getElementById("film-card-template")
@@ -18,7 +37,7 @@ function makePageForEpisodes(episodeList) {
     filmCard.getElementById("summary").innerHTML = episode.summary;
     filmCard.querySelector("img").src = episode.image.medium;
 
-    document.body.appendChild(filmCard);
+    main.appendChild(filmCard);
   }
 }
 
