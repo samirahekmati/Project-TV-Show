6811a38ad8e184eb3eee280eb7b519889ea65401
samirahekmati <samirahekmati@gmail.com>
2025-01-17 15:19:02 +0000

    cleaner code

--- script.js
+++ script.js
@@ -1,66 +1,18 @@
-//You can edit ALL of the code here
-const inputEl = document.getElementById("input-el");
-const searchCount = document.querySelector("#results-count");
-const showOption = document.getElementById('select-show');
+const inputEl = document.getElementById("input-el"); //Level 200
+const searchCount = document.querySelector("#results-count"); // Level 200
+const showOption = document.getElementById("select-show"); //Level 400
 
 let allEpisodes = [];
 let allShows = [];
 
 async function setup() {
-  // allEpisodes = await getAllEpisodes();
   allShows = await getAllShows();
   searchEpisodes();
   selectEpisode();
   makePageForEpisodes(allEpisodes);
-  // updateEpisodeCount(allEpisodes.length, allEpisodes.length);
 }
 
-async function getAllShows() {
-  if (allShows.length > 0) return allShows;
-
-  const response = await fetch('https://api.tvmaze.com/shows');
-  const data = await response.json();
-  console.log(data);
-  allShows = data.sort((a, b) => a.name.localeCompare(b.name));
-  
-  const defaultOpt = document.createElement('option');
-  defaultOpt.value = 'all';
-  defaultOpt.textContent = 'Select a show..';
-  showOption.appendChild(defaultOpt);
-  allShows.forEach(show => {
-    const option = document.createElement('option');
-    option.value = show.id;
-    option.textContent = show.name;
-    showOption.appendChild(option);
-  });
-
-  showOption.addEventListener('change', (event) => {
-    if (event.target.value === 'all') {
-      makePageForEpisodes();
-    }
-    else {
-      getAllEpisodes(event.target.value);
-    }
-  })
-}
-
-async function searchEpisodes() {
-  inputEl.addEventListener("input", () => {
-    const selected = document.getElementById('selected');
-    selectEpisode();
-    const searchedWord = inputEl.value.toLowerCase();
-    const matchingEpisodes = allEpisodes.filter((episode) => {
-      return (
-        episode.name.toLowerCase().includes(searchedWord) ||
-        episode.summary.toLowerCase().includes(searchedWord)
-      );
-    });
-
-    makePageForEpisodes(matchingEpisodes);
-    updateEpisodeCount(matchingEpisodes.length, allEpisodes.length);
-  });
-}
-
+////Level 100////
 function makePageForEpisodes(episodeList) {
   const main = document.getElementById("all-episodes");
   main.innerHTML = "";
@@ -82,10 +34,35 @@ function makePageForEpisodes(episodeList) {
   }
 }
 
+/// Level 200: Add a live search input ///
+
+async function searchEpisodes() {
+  inputEl.addEventListener("input", () => {
+    selectEpisode();
+    const searchedWord = inputEl.value.toLowerCase();
+    const matchingEpisodes = allEpisodes.filter((episode) => {
+      return (
+        episode.name.toLowerCase().includes(searchedWord) ||
+        episode.summary.toLowerCase().includes(searchedWord)
+      );
+    });
+
+    makePageForEpisodes(matchingEpisodes);
+    updateEpisodeCount(matchingEpisodes.length, allEpisodes.length);
+  });
+}
+
+//Level 200//
+//Display how many episodes match the current search
+const updateEpisodeCount = (episodeCount, totalEpisodes) => {
+  searchCount.textContent = `Matching episodes: ${episodeCount} / ${totalEpisodes}`;
+};
+
+/// Level 200: Episode selector//
 const selectEpisode = async () => {
   // allEpisodes = await getAllEpisodes();
   const select = document.getElementById("select-episode");
-  select.innerHTML = '';
+  select.innerHTML = "";
 
   allEpisodes.forEach((episode) => {
     const option = document.createElement("option");
@@ -97,8 +74,8 @@ const selectEpisode = async () => {
   });
 
   select.addEventListener("change", (ev) => {
-    const inputel = document.getElementById('input-el');
-    inputel.value = ""
+    const inputel = document.getElementById("input-el");
+    inputel.value = "";
     if (ev.target.value === "all") {
       makePageForEpisodes(allEpisodes);
     } else {
@@ -106,14 +83,12 @@ const selectEpisode = async () => {
         (epsd) => epsd.id == ev.target.value
       );
       makePageForEpisodes([selectEpisode]);
-      updateEpisodeCount([selectEpisode].length,[selectEpisode].length)
+      updateEpisodeCount([selectEpisode].length, [selectEpisode].length);
     }
   });
 };
 
-const updateEpisodeCount = (episodeCount, totalEpisodes) => {
-  searchCount.textContent = `Matching episodes: ${episodeCount} / ${totalEpisodes}`;
-};
+// Level 300//
 
 async function getAllEpisodes(showID) {
   const url = `https://api.tvmaze.com/shows/${showID}/episodes`;
@@ -122,14 +97,43 @@ async function getAllEpisodes(showID) {
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
     }
-    allEpisodes = await response.json(); 
+    allEpisodes = await response.json();
     makePageForEpisodes(allEpisodes);
-    selectEpisode()
+    selectEpisode();
   } catch (error) {
     console.error(error.message);
-  
   }
-  
+}
+
+///// Level 400:Add a `select` element to your page so the user can choose a show.
+
+async function getAllShows() {
+  if (allShows.length > 0) return allShows;
+
+  const response = await fetch("https://api.tvmaze.com/shows");
+  const data = await response.json();
+  console.log(data);
+  allShows = data.sort((a, b) => a.name.localeCompare(b.name));
+
+  const defaultOpt = document.createElement("option");
+  defaultOpt.value = "all";
+  defaultOpt.textContent = "Select a show..";
+  showOption.appendChild(defaultOpt);
+  allShows.forEach((show) => {
+    const option = document.createElement("option");
+    option.value = show.id;
+    option.textContent = show.name;
+    showOption.appendChild(option);
+  });
+
+  showOption.addEventListener("change", (event) => {
+    
+    if (event.target.value === "all") {
+      makePageForEpisodes();
+    } else {
+      getAllEpisodes(event.target.value);
+    }
+  });
 }
 
 window.onload = setup;
